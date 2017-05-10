423e359f1e514ccfb9c35302d05ac62c519fcd49
Raden Miftakhurozak Budi Nugraha <16523119@students.uii.ac.id>
2017-05-10 15:02:16 +0700

    Update CobaTortoise.java

--- Coba Tortoise/src/coba/tortoise/CobaTortoise.java
+++ Coba Tortoise/src/coba/tortoise/CobaTortoise.java
@@ -17,6 +17,7 @@ public class CobaTortoise {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Coba Compile");
+        System.out.println("Ini aja")
     }
     
 }
